5871a13837a9e11ea1c04afc8bfc1b4b049604c3
Dmitry Kosenko <ukrkosenko@gmail.com>
2024-06-25 15:28:21 +0300

    add new

--- lib/services/weather_api_provider.dart
+++ lib/services/weather_api_provider.dart
@@ -7,7 +7,7 @@ import '../models/weather.dart';
 class WeatherProvider {
   Future<Weather> getWeather() async {
     var url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?appid=0c2d6189e0ec193fc49c5fffd923c75c&q=kyiv&units=metric&lang=ua');
+        'https://stores-api.zakaz.ua/stores/48201070/products/search/?q=4820017000543&per_page=10');
     final response = await http.get(url);
     print(response.statusCode);
     if (response.statusCode == 200) {
